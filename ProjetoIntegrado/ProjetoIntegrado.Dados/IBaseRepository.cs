7fd00c564666fe0cda55dbc1ed285d2f6185b08c
wellington.sales <wellington.sales@INT73>
2019-07-26 18:22:50 -0300

    Menu removido, alteração de validação no web.config e namespace de BaseRepository

--- ProjetoIntegrado/ProjetoIntegrado.Dados/IBaseRepository.cs
+++ ProjetoIntegrado/ProjetoIntegrado.Dados/IBaseRepository.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
-namespace ProjetoIntegrado.Dados.Repositorios
+namespace ProjetoIntegrado.Dados
 {
 
     public interface IBaseRepository<TEntidade, TChave> where TEntidade : class
